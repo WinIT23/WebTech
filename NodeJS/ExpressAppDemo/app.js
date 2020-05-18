36a76404acd13caa20a55d19465d28a32049dc00
WinIT23 <chauhanvinit23@gmail.com>
2020-05-18 15:53:26 +0530

    Express App 1

--- NodeJS/ExpressAppDemo/app.js
+++ NodeJS/ExpressAppDemo/app.js
@@ -1,21 +1,33 @@
 var express = require('express');
 var app = express();
 
+app.get("/r/:subRedditName", (req, res) => { // -- :sRN means pattern
+    var subreddit = req.params.subRedditName;
+    res.send("WELCOME TO A r/" + subreddit);
+});
+
+app.get("/r/:subRedditName/comments/:id/:title", (_req, res) => {
+    res.send("WELCOME TO COMMENTS PAGE");
+});
 
 // "/" -> "Hi There"
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
     res.send("Hi There");
 });
 
 // "/bye" -> "Bbye"
-app.get("/bye", (req, res) => {
+app.get("/bye", (_req, res) => {
     res.send("Bbye!!");
 });
 // "/dog" -> "Woofff"
-app.get("/dog", (req, res)=> {
+app.get("/dog", (_req, res) => {
     console.log("SOMEONE MADE REQUEST ON /DOG");
-    
+
     res.send("Wooff.... Woofff!!!");
+});
+
+app.get("*", (_req, res) => {
+    res.send("Check URL..");
 });
 
 // Tell Express to listen for request (start Server)
