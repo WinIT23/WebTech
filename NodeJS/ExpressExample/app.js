38744c0805ce4f2f4fd0d9af28cea7dcf212eb61
WinIT23 <chauhanvinit23@gmail.com>
2020-07-30 18:51:24 +0530

    Express Exmaple complete

--- NodeJS/ExpressExample/app.js
+++ NodeJS/ExpressExample/app.js
@@ -1,15 +1,45 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+
+// set resource folder
+app.use(express.static("public"));
+// we don't have to write .ejs in every render statement.
+app.set("view engine", "ejs");
 
 app.get("/", (_req, res) => {
-    res.render("index.ejs");
+    res.render("index");
 });
 
 app.get("/msg/:thing", (req, res) => {
-    var thing = req.params.thing;
-    res.render("things.ejs", {thingVar: thing})
+    res.render("things", {
+        thingVar: req.params.thing
+    })
+});
+
+app.get("/speak", (_req, res) => {
+    var Animals = [{
+        name: "pig",
+        sound: "Oink"
+    }, {
+        name: "cow",
+        sound: "Moo"
+    }, {
+        name: "dog",
+        sound: "Woof Woof!"
+    }, {
+        name: "cat",
+        sound: "Meow"
+    }, {
+        name: "goat",
+        sound: "Meeehhhh"
+    }];
+
+    res.render("speak", {
+        animalArr: Animals
+    });
 });
 
 var port = 3000;
 app.listen(port, () => {
-    console.log("SERVER IS RUNNING ON PORT " + port); 
+    console.log("SERVER IS RUNNING ON PORT " + port);
 });
